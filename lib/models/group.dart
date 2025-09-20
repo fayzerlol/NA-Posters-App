f64319bc2971da24ed7fcfd3cf02f90df86e8c51
fayzerrennan <fayzer.rennan@gmail.com>
2025-09-19 21:03:05 -0700

    feat: add NaGroup model

---
+++ lib/models/group.dart
@@ -0,0 +1,14 @@
+class NaGroup {
+  final String id;
+  final String name;
+
+  NaGroup({required this.id, required this.name});
+}
+
+final List<NaGroup> naGroups = [
+  NaGroup(id: 'sp_barroca_bh', name: 'Só por Hoje – Barroca, BH'),
+  NaGroup(id: 'sp_centro_sp', name: 'Grupo Centro – SP'),
+  NaGroup(id: 'sp_vila_madalena', name: 'Grupo Vila Madalena – SP'),
+  NaGroup(id: 'rj_centro', name: 'Grupo Centro – RJ'),
+  NaGroup(id: 'rs_porto_alegre', name: 'Grupo Porto Alegre – RS'),
+];
