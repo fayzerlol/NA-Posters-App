29d0b9e05c81d9d3fbed7b1220948958194145b5
rennan.fayzer <rennan.fayzer@gmail.com>
2025-09-21 05:14:13 +0000

    Changes before Firebase Studio auto-run

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -9,8 +12,30 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Mapa'),
       ),
-      body: const Center(
-        child: Text('Map view goes here'),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(51.509364, -0.128928),
+          initialZoom: 9.2,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+            additionalOptions: {
+              'accessToken': dotenv.env['MAPBOX_ACCESS_TOKEN']!,
+              'id': 'mapbox/streets-v11',
+            },
+          ),
+          const MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(51.509364, -0.128928),
+                width: 80,
+                height: 80,
+                child: FlutterLogo(),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
